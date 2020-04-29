0d7cf939004eac0bb5587ccd0bd8c84070723bed
evoA <evoa@foxmail.com>
2020-04-29 11:33:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
+evoAList = []
 function getVideo(document,documentStr){
-    
+	    
     if (document.getElementsByTagName('video').length === 0) return false
     var len = document.getElementsByTagName('video').length
     for(var i=0;i<len;i++){
-        console.log(`${documentStr}.getElementsByTagName('video')[${i}].playbackRate=15`)
-        console.log(`${documentStr}.getElementsByTagName('video')[${i}].currentTime =600`)
-    }
+                console.log(`${documentStr}.getElementsByTagName('video')[${i}].pause=function(){return false}`)
+                console.log(`${documentStr}.getElementsByTagName('video')[${i}].playbackRate=15`)
+                console.log(`${documentStr}.getElementsByTagName('video')[${i}].currentTime =600`)
+            }
 }
 
 function getAllIframe(document,documentStr){
-    evoAList = []
+    
     if (document.getElementsByTagName('iframe').length === 0) return false
     var len = document.getElementsByTagName('iframe').length
     for(var i=0;i<len;i++){
+
+        
+
         if(document.getElementsByTagName('iframe')[i].contentDocument === null) return false
         getAllIframe(document.getElementsByTagName('iframe')[i].contentDocument,`${documentStr}.getElementsByTagName('iframe')[${i}].contentDocument`)
 
-
         evoAList.push({document:document.getElementsByTagName('iframe')[i].contentDocument,documentStr:`${documentStr}.getElementsByTagName('iframe')[${i}].contentDocument`})
+                
 
-    }
+        }
     return evoAList
 }
 
@@ -29,10 +34,10 @@ function getAllIframe(document,documentStr){
     var docuStr
     var docu
     for(var i=0;i<evoAList.length;i++){
-        docuStr = evoAList[i].documentStr
-        docu = evoAList[i].document
-        getVideo(docu,docuStr)
-        
-    }
+                docuStr = evoAList[i].documentStr
+                docu = evoAList[i].document
+                getVideo(docu,docuStr)
+                
+            }
     
 })()
